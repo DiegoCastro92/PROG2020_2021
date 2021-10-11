67dd492275f7d20be3fd000cc1e9245a5c736b37
DiegoCastro92 <dcastro2292@gmail.com>
2021-10-11 10:54:24 +0200

    Create PROG01_programa1.java

---
+++ PROG01_programa1.java
@@ -0,0 +1,12 @@
+class PROG01_programa1{
+
+    public static void main(String[] args) {
+   	System.out.println("Modulo Profesional - PROGRAMACION. UNIDAD DE TRABAJO 01");
+	System.out.println("Introduccion a la programacion");
+	System.out.println("Diego Castro Fernandez");
+	System.out.println("Camargo, Cantabria");
+	System.out.println("05/10/2021");
+	System.out.println("Programa1");
+    }
+
+}
